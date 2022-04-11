b1e3535b8b2fcd1c172a5c5558aeb6f8ca74b6ec
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2022-04-11 20:45:10 +0200

    feat: adding dresses category and dress page

---
+++ pages/clothes/robes/index.js
@@ -0,0 +1,41 @@
+import Default from "../../../components/layouts/Default";
+import axios from "axios"
+import Robes from "../../../components/clothes/robes";
+
+export default function Dresses({clothes}) {
+    return (
+        <div>
+            <Default>
+                <section className="bg-beige w-full">
+                    <header className="p-14">
+                        <h1 className="text-center text-3xl font-link">Robes</h1>
+                    </header>
+                    <Robes clothes={clothes}/>
+                </section>
+            </Default>
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    //Chercher des infos dans l'api
+    const res = await axios.get("http://localhost:3000/clothes/collections/Robe")
+    const clot = res.data.map(clothe => {
+        return {
+            id: clothe._id,
+            name: clothe.name,
+            brand: clothe.brand,
+            price: clothe.price,
+            description: clothe.description,
+            category: clothe.category,
+            condition: clothe.condition,
+            color: clothe.color,
+            image: clothe.image
+        }
+    });
+    return {
+        props: {
+            clothes: clot,
+        }
+    }
+}
