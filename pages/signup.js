638ccb23ee59434c8c1d25e72ffeced59f8ab91b
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2022-04-11 09:26:58 +0200

    feat: SignUp

---
+++ pages/signup.js
@@ -0,0 +1,12 @@
+import Default from "../components/layouts/Default";
+import SignUp from "../components/layouts/SignUp";
+
+export default function Signup() {
+  return (
+      <div>
+        <Default>
+          <SignUp/>
+        </Default>
+      </div>
+  )
+}
